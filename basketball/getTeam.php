322bf6266f639f9577a3109090ab495571352bf8
Woody Wu <Woody Wu>
2016-01-20 00:14:33 +0800

    fix

--- basketball/getTeam.php
+++ basketball/getTeam.php
@@ -7,6 +7,10 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    if (!$conn->set_charset("utf8")) {
+      printf("Error loading character set utf8: %s\n", $conn->error);
+  }
+
     $table = "basketballteam";          //TODO: change this with different ball game team
     $sql = "SELECT id,nickName FROM " . $table . " where type=1";
     $result = mysqli_query($conn, $sql);
